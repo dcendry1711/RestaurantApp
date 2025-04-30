ac2d5398bd7e63981c53d5788d5cf45010095ffb
dcendry1711 <daniel.cendry@gmail.com>
2025-04-30 09:09:59 +0200

    update: add order to array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import {menuArray} from './data.js'
 
 const mainMenu = document.getElementById('restaurant-menu')
+const clientOrder = document.getElementById('restaurant-order')
 
 function renderMenu(menuArr){
     let html = ''
@@ -12,13 +13,13 @@ function renderMenu(menuArr){
                 <div class="emoji">
                     <img src="${meal.emoji}">
                 </div>
-                <div class="menu-info">
+                <div class="menu-info" id="${meal.name}>
                     <p class="bold">${meal.name}</p> 
                     <p class="light">${meal.ingredients}</p>
                     <p class="bold">$${meal.price}
                 </div>
                 <div>
-                    <button id="${meal.name}">+</button>
+                    <button class="btn" data-meal="${meal.name}">+</button>
                 </div>
             </div>
         </section>
@@ -28,3 +29,14 @@ function renderMenu(menuArr){
 }
 
 mainMenu.innerHTML = renderMenu(menuArray)
+
+
+const orderArray = []
+
+mainMenu.addEventListener('click',function(e){
+    if(e.target.dataset.meal){
+        clientOrder.style.visibility = 'visible'
+        orderArray.push(e.target.dataset.meal)
+        console.log(orderArray)
+    }
+})
